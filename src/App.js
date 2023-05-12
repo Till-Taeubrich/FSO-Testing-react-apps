ccca5d7141f760fad2e1dd2c16b71db85783493e
Till Taeubrich <till.taeubrich@gmail.com>
2023-05-12 16:22:59 +1000

    finish exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    
+    try {
+      const user = await loginService({ username, password })
+      console.log(user);
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log('Wrong credentials');
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div class="username-input">
+        Username
+        <input
+          type="text"
+          value={username}
+          name='Username'
+          onChange={({ target }) => setUsername(target.value)}
+          />
+      </div>
+      <div class="password-input">
+        Password
+        <input
+          type="text"
+          value={password}
+          name='Password'
+          onChange={({ target }) => setPassword(target.value)}
+          />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +53,19 @@ const App = () => {
     )  
   }, [])
 
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <div>{ user.username } logged in</div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
