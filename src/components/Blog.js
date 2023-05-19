809060ee3e81d25742b6382bfc9c845bceb5dd23
Till Taeubrich <till.taeubrich@gmail.com>
2023-05-19 15:29:57 +1000

    finish exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({
+  blog
+}) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showAllInfo, setShowAllInfo] = useState(false)
+
+  const showWhenVisible = { display: showAllInfo ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+      <div className="initially-hidden" style={showWhenVisible} >
+        <div>
+          {blog.url} 
+        </div>
+        <div>
+          {blog.likes} 
+        </div>
+        <div>
+          {blog.username} 
+        </div>
+      </div>
+      <button onClick={() => setShowAllInfo(!showAllInfo)}>view</button>
+    </div>
+  )
+}
 
 export default Blog
