b73ab40dffd8986a98867618e4e45628d2ef4181
Till Taeubrich <till.taeubrich@gmail.com>
2023-06-14 22:55:56 +0900

    finish exercise 5.12

--- src/components/NotificationBar.js
+++ src/components/NotificationBar.js
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types'
+
 const NotificationBar = ({ notification }) => {
   return(
     <div>
       { notification }
     </div>
   )
+}
+
+
+NotificationBar.propTypes = {
+  notification: PropTypes.string.isRequired
 }
 
 export default NotificationBar
