b68cfac381815e6ddac919d8d89f697c37b6dcc0
Till Taeubrich <till.taeubrich@gmail.com>
2023-07-23 12:37:24 +0200

    finish exercise 5.20

--- cypress/e2e/bloglist.cy.js
+++ cypress/e2e/bloglist.cy.js
@@ -49,11 +49,11 @@ describe('Blog app', function() {
       cy.visit('http://localhost:3000')
     })
 
-    it.only('A blog can be created', function() {
+    it('A blog can be created', function() {
       cy.addBlog()
     })
 
-    it.only('A blog can be liked', function() {
+    it('A blog can be liked', function() {
       cy.addBlog().then(res => {
         const blog = res.body
         const blogId = res.body.id
@@ -66,6 +66,26 @@ describe('Blog app', function() {
           expect(res.body[0].likes).to.eq(2)
         })
       })
+    })
+
+    it('A blog can be deleted by the author', function() {
+
+      cy.addBlog().then(res => {
+        const blogId = res.body.id
+
+        const token = `Bearer ${JSON.parse(localStorage.getItem('loggedUser')).token}`
+
+        const config = {
+          Authorization: token
+        }
+
+        cy.request({
+          method: 'DELETE',
+          url: `http://localhost:3003/api/blogs/${blogId}`,
+          headers: config
+        })
+      })
+
     })
   })
 })
